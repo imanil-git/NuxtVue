e6a18bdc406e5d4c2bba58b2e327e7fdff751fca
Anil Rumdali Rai <anil.rai704757@gmail.com>
2024-03-17 16:23:18 +0545

    Video Card Added

--- pages/index.vue
+++ pages/index.vue
@@ -46,7 +46,9 @@
       </p>
       <!-- Upcoming Classes Card -->
       <div class="flex gap-16 overflow-scroll">
-        <Upcomingclass />
+        <div v-for="i in 4" :key="i">
+          <Upcomingclass />
+        </div>
       </div>
       <!-- End of Upcoming Classes Card -->
     </div>
@@ -124,11 +126,63 @@
 
     <!-- Popular Courses Card  -->
     <div class="p-10 grid grid-cols-4 gap-6">
-        <Popularcard />
+      <Popularcard />
     </div>
     <!-- End of Popular Courses Card -->
   </section>
   <!-- End of Popular Courses -->
+
+  <!-- Feedback -->
+  <section class="bg-[#2E5093] mb-20">
+    <div class="relative container mx-auto py-6">
+      <h1 class="text-center text-2xl font-bold mb-12 text-white">
+        What our students say about us
+      </h1>
+      <div class="flex gap-16 justify-between overflow-scroll">
+        <div v-for="i in 10" :key="i">
+          <Video />
+        </div>
+      </div>
+      <div
+        class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2"
+      >
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full bg-white"
+          aria-current="true"
+          aria-label="Slide 1"
+          data-carousel-slide-to="0"
+        ></button>
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full bg-white"
+          aria-current="true"
+          aria-label="Slide 1"
+          data-carousel-slide-to="1"
+        ></button>
+        <button
+          type="button"
+          class="w-3 h-3 rounded-full bg-white"
+          aria-current="true"
+          aria-label="Slide 1"
+          data-carousel-slide-to="2"
+        ></button>
+      </div>
+    </div>
+  </section>
+  <!-- End of Feedback -->
+
+  <!-- About Partner -->
+  <section class="container mx-auto mb-9">
+    <h1 class="text-center text-2xl font-bold mb-2">Our Placement Partner</h1>
+    <p class="mb-6 indent-4 text-center">At CodeIt, we take immense pride in our strong network of placement partners, which plays a pivotal role in shaping the success stories of our talented <br> individuals. These strategic collaborations with industry leaders and renowned companies enable us to provide unparalleled placement opportunities for our students.</p>
+    <div class="flex gap-5 justify-between">
+      <div v-for="i in 4" :key="i">
+        <img class-="w-64 h-64" src="../assets/images/mantra.png" alt="">
+      </div>
+    </div>
+  </section>
+  <!-- End of About Partner -->
 </template>
 
 <script setup></script>
